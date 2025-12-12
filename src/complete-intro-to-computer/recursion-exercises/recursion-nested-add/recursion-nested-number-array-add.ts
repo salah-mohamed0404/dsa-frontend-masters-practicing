e5a6ba04fdf9ad14bef6a4217eb591adea0e0e16
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-12-12 21:49:39 +0200

    docs: add JSDoc comments for recursionNestedNumberArrayAdd function

--- src/complete-intro-to-computer/recursion-exercises/recursion-nested-add/recursion-nested-number-array-add.ts
+++ src/complete-intro-to-computer/recursion-exercises/recursion-nested-add/recursion-nested-number-array-add.ts
@@ -1,5 +1,11 @@
 type NestedNumberArray = Array<number | NestedNumberArray>;
 
+/**
+ * Recursively adds all numbers in a nested array structure.
+ * @param arr - A nested array of numbers.
+ * @returns The sum of all numbers in the nested array.
+ */
+
 export function recursionNestedNumberArrayAdd(arr: NestedNumberArray): number {
 	let sum = 0;
 
